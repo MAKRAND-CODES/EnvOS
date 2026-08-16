ec5063c043e00e2dbb038d8d8f73f271d95f9c62
Makrand <makrandsanghvi124@gmail.com>
2026-08-16 16:22:37 +0530

    Learn Python variables

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ numbers = np.array([10,20,30,40])
 
 print(numbers)
 print(numbers.mean())
+
+city="Indore"
+aqi=145
+temperature=32.5
+humidity=68
+wind_speed = 12.5
+
+print(city)
+print(aqi)
+print(temperature)
+print(humidity)
+print(type(city))
+print(type(aqi))
+print(type(temperature))
+print(type(humidity))
+print(wind_speed)
