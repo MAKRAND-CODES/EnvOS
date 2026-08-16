d451e56f7d26b6a63d46654daa88a9b442d8b610
Makrand <makrandsanghvi124@gmail.com>
2026-08-16 16:49:09 +0530

    Leanr Python Strings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Welcome to EnvOS")
+"""print("Welcome to EnvOS")
 print("Environmental Operating System")
 
 import numpy as np;
@@ -23,3 +23,39 @@ print(type(aqi))
 print(type(temperature))
 print(type(humidity))
 print(wind_speed)
+"""
+
+#city1 = "Indore"
+#city2 = "Bhopal"
+
+#print(city1)
+#print(city2)
+
+state = "Bhopal"
+city = "Indore"
+
+print(city)
+print(state)
+
+print(city + "     " + state)
+print(len(city)) #length using len function
+
+print(city[0])
+print(city[1])
+print(city[2])
+print(city[3])
+print(city[4])
+print(city[-1])
+
+#String methods
+
+city = "Bhopal"
+aqi = 140
+temperature = 35
+print(f"{city} has an AQI of {aqi} and temperature of {temperature}C")
+
+'''print(city.upper())
+print(city.capitalize())
+print(city.strip())'''
+
+
